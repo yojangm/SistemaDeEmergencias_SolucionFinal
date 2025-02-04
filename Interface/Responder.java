6fa6f47395ea81be22f1a9b918bca1b8f2b9f595
Brayan Steven Perez Hernandez <brayanstevenp16@gmail.com>
2025-02-04 15:07:28 -0500

    InterfaceEmergencia

---
+++ Interface/Responder.java
@@ -0,0 +1,4 @@
+public interface Responder {
+    void atenderEmergencia(Emergencia emergencia); // Método para atender una emergencia
+    void evaluarEstado(); // Método para ver cómo están los servicios
+}
