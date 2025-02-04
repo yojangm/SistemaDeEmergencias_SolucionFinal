09c2ace2c2f72e5cc7224283f04795f97779f891
Brayan Steven Perez Hernandez <brayanstevenp16@gmail.com>
2025-02-04 15:13:29 -0500

    Clase Ambulancia

---
+++ Ambulancia.java
@@ -0,0 +1,11 @@
+public class Ambulancia implements Responder{
+    @Override
+    public void atenderEmergencia(Emergencia emergencia) {
+        System.out.println("Ambulancia atendiendo emergencia: " + emergencia.getTipo() + " en " + emergencia.getUbicacion());
+    }
+
+    @Override
+    public void evaluarEstado() {
+        System.out.println("Evaluando estado de las ambulancias...");
+    }
+}
