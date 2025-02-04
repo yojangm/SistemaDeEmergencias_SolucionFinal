609574451c3817ccbe212fe3b1cb87f99b684418
Brayan Steven Perez Hernandez <brayanstevenp16@gmail.com>
2025-02-04 15:34:56 -0500

    Factory

---
+++ Factory/EmergenciaFactory.java
@@ -0,0 +1,11 @@
+package Factory;
+
+import Strategy.Emergencia;
+
+public class EmergenciaFactory {
+    
+    public Emergencia crearEmergencia(String tipo, String ubicacion, int gravedad) {
+        return new Emergencia(tipo, ubicacion, gravedad);
+    }
+
+}
